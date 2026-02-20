09ce800525322683ac61b81749db89c3f27197ab
kbbb36 <kilian.malek@gmail.com>
2026-02-20 11:11:18 +0100

    cleanup

--- src/fragment_ion_index.cpp
+++ src/fragment_ion_index.cpp
@@ -21,32 +21,6 @@
 
 using namespace std;
 
-
-fragment_ion_index::fragment_ion_index() {
-
-}
-
-
-fragment_ion_index::fragment_ion_index(precursor_index *parent_index) {
-
-    /*
-    * todo
-
-    fragment_bins = vector<fragment_bin>(BIN_MAX_MZ + 1); //TODO remove/determine actual max #bins
-    for (int i = 0; i < parent_index->get_size(); ++i) {
-        spectrum *c_spectrum = parent_index->get_spectrum(i);
-
-
-        //Iterate all peaks and save them as fragments in the corresponding ion mz bin
-        for (int j = 0; j < c_spectrum->binned_peaks.size(); ++j) {
-            int bin = c_spectrum->binned_peaks[j];
-            fragment frag(c_spectrum->id, c_spectrum->binned_intensities[j]);
-            fragment_bins[bin].push_back(frag);
-        }
-    }
-    */
-}
-
 fragment_ion_index::fragment_ion_index(string path) : file_path(path) {
 
     //load_index_from_file(file_path);
@@ -77,59 +51,6 @@ bool fragment_ion_index::sort_index(std::unique_ptr<precursor_index>& parent_ind
         }
     }
 
-    return true;
-}
-
-bool fragment_ion_index::load_index_from_file(const std::string& path) {
-
-    /*
-     * Read index from file
-     */
-
-    ifstream f(path, ios::in);
-    string delimiter = ";";
-
-    fragment_bins.clear();
-    fragment_bins.resize(BIN_MAX_MZ + 1);
-
-    string line;
-    while (getline(f,line)) {
-        size_t delim_pos = line.find(delimiter);
-        size_t delim_right_pos = line.rfind(delimiter);
-
-        unsigned int id = stoi(line.substr(0, delim_pos));
-        int mz_bin = stoi(line.substr(delim_pos + 1, delim_right_pos - delim_pos - 1));
-        float intensity = stof(line.substr(delim_right_pos + 1, string::npos));
-
-        fragment_bins[mz_bin].emplace_back(fragment(id, intensity));
-
-    }
-
-
-    f.close();
-    return true;
-}
-
-bool fragment_ion_index::save_index_to_file(const string &path) {
-
-    /*
-     * TODO: OBSOLETE using binary file reader
-     */
-
-    ofstream f(path, ios::out);
-    //f.precision(FLOAT_OUTPUT_PRECISION);
-
-    string delimiter = ";";
-
-    for (int i = 0; i < fragment_bins.size(); ++i) {
-
-        fragment_bin  bin = fragment_bins[i];
-        for (auto & j : bin) {
-            f << j.parent_id << delimiter << i << delimiter << j.intensity << "\n";
-        }
-    }
-
-    f.close();
     return true;
 }
 
